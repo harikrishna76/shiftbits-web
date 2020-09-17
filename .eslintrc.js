6175914e59923fc4ce469b39365fe99f37ecc4ec
harikrishna <harikrishna.salver@gmail.com>
2020-09-17 12:50:02 +0530

    lint setup

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// ESLint configuration
+// http://eslint.org/docs/user-guide/configuring
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [0],
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
