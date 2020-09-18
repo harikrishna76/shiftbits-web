eec2477651f2e248576717914e2e44a0794d2888
harikrishna <harikrishna.salver@gmail.com>
2020-09-18 23:07:15 +0530

    page title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import 'styles/globals.css';
 
 function MyApp(props) {
@@ -11,6 +12,10 @@ function MyApp(props) {
 
   return (
     <div style={{ display: showUI ? 'block' : 'none' }}>
+      <Head>
+        <title>ShiftBits - Coming Soon</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
