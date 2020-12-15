dfbff3157329a8417a285946e2ab57d4b4ac4e98
harikrishna.salver@gmail.com <harikrishna.salver@gmail.com>
2020-12-16 01:08:19 +0530

    spends page completed

---
+++ src/pages/spends/constants.js
@@ -0,0 +1,62 @@
+const FeaturedCards = ['paytm', 'amazon', 'googlePlay', 'flipkart', 'airtel'];
+
+const AllGiftCards = {
+  paytm: {
+    key: 'paytm',
+    site: 'Paytm.in',
+    logo: '/images/paytm_1.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+  googlePlay: {
+    key: 'googlePlay',
+    site: 'GooglePlay.in',
+    logo: '/images/google.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+  bigbasket: {
+    key: 'bigbasket',
+    site: 'BigBasket.in',
+    logo: '/images/bigbasket.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+  flipkart: {
+    key: 'flipkart',
+    site: 'Flipkart.in',
+    logo: '/images/flipkart.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+  amazon: {
+    key: 'amazon',
+    site: 'Amazon.in',
+    logo: '/images/amazon_in.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+  skype: {
+    key: 'skype',
+    site: 'Skype.in',
+    logo: '/images/skype.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+  nike: {
+    key: 'nike',
+    site: 'Nike.in',
+    logo: '/images/amazon_in.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+  airtel: {
+    key: 'airtel',
+    site: 'Airtel.in',
+    logo: '/images/airtel.svg',
+    description:
+      'Convert your Bitcoin to Amazon.in gift cards for books, music, DVDs, electronics, computers, software, and everything you need to live on crypto.',
+  },
+};
+
+export {AllGiftCards, FeaturedCards};
--- src/pages/spends/index.js
+++ src/pages/spends/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Typography, Hidden } from '@material-ui/core';
 import ProductLayout from 'components/product-layout';
+import { AllGiftCards, FeaturedCards } from './constants';
 import s from './Spends.module.scss';
 
 export default function Spends() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const renderHeader = () => {
+    return (
+      <div className={`${s.headerSection} spaceBetween`}>
+        <div className={s.title}>
+          {searchValue
+            ? `Search results for “${searchValue}”`
+            : 'All gift cards'}
+        </div>
+        <div>
+          <input
+            className={s.searchInput}
+            type="text"
+            placeholder="Search for gift cards"
+            onChange={(event) => {
+              setSearchValue(event.target.value);
+            }}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const displayAllGiftCards = () => {
+    let cards = Object.values(AllGiftCards);
+    if (searchValue) {
+      cards = cards.filter((card) => {
+        return card.key.search(new RegExp(searchValue, 'i')) !== -1;
+      });
+    }
+    return (
+      <div>
+        <div className="flexWrap spaceBetween">
+          {cards.map((giftCard) => (
+            <div className={`${s.card} center`}>
+              <img src={giftCard.logo} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <ProductLayout activeNavigation="spends">
-      <Grid container alignItems="center" style={{ padding: '2rem 2rem 1rem' }}>
-        <Grid item xs={12} md={6}>
-          <div style={{ textAlign: 'center' }}>Spends</div>
+      <Grid container style={{ padding: '2rem 2rem 1rem' }}>
+        <Grid item xs={12} md={11}>
+          {renderHeader()}
+          {displayAllGiftCards()}
         </Grid>
       </Grid>
     </ProductLayout>
